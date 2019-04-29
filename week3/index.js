a2fdb1d3c16377cba116861471a601bbbae24965
Vladyslav <kytsevolov@gmail.com>
2019-04-28 18:14:21 -0600

    js snippet

--- week3/index.js
+++ week3/index.js
@@ -1,74 +1,94 @@
-/**
- * @param {String} date
- * @returns {Object}
- */
 module.exports = function (date) {
-let time = new Date(date);
-let timeObjects = ['years', 'months', 'days', 'hours', 'minutes'];
+    var newDate = new Date(date);
+    var dateVal = ['years', 'months', 'days', 'hours', 'minutes'];
+    var time = {
 
-return {
+            add: function (amount, name) {
+                if (amount < 0) {
+                    throw new TypeError();
+                }
+                if (!dateVal.includes(name)) {
+                    throw new TypeError();
+                }
+                if (name === 'years') {
+                    newDate.setFullYear(newDate.getFullYear() + amount);
 
-	add(num, val) {
-			if(!timeObjects.includes(val) || (num < 0) || isNaN(num)) {
-				throw new TypeError('Invalid input');
-		} else if (val === 'years') {
-			time.setFullYear(time.getFullYear() + num); 
-		} else if (val === 'months') {
-			time.setMonth(time.getMonth() + num); 
-		} else if (val === 'days') {
-			time.setDate(time.getDate() + num); 
-		} else if (val === 'hours') {
-			time.setHours(time.getHours() + num); 
-		} else if (val === 'minutes') {
-			time.setMinutes(time.getMinutes() + num); 
-		} 
-		return this;
-	},
+                }
+                if (name === 'months') {
+                    newDate.setMonth(newDate.getMonth() + amount);
+
+                }
+                if (name === 'days') {
+                    newDate.setDate(newDate.getDate() + amount);
+
+                }
+                if (name === 'hours') {
+                    newDate.setHours(newDate.getHours() + amount);
+
+                }
+                if (name === 'minutes') {
+                    newDate.setMinutes(newDate.getMinutes() + amount);
+                }
+                return this;
+            },
+
+            subtract: function (amount, name) {
+                if (amount < 0) {
+                    throw new TypeError();
+                }
+                if (!dateVal.includes(name)) {
+                    throw new TypeError();
+                }
+                if (name === 'years') {
+                    newDate.setFullYear(newDate.getFullYear() - amount);
+
+                }
+                if (name === 'months') {
+                    newDate.setMonth(newDate.getMonth() - amount);
+
+                }
+                if (name === 'days') {
+                    newDate.setDate(newDate.getDate() - amount);
+
+                }
+                if (name === 'hours') {
+                    newDate.setHours(newDate.getHours() - amount);
+
+                }
+                if (name === 'minutes') {
+                    newDate.setMinutes(newDate.getMinutes() - amount);
+
+                }
+                return this;
+            },
+            get value() {
+            	var res = newDate,
+                	year = res.getFullYear(),
+                	month = res.getMonth() +1,
+                	day = res.getDate(),
+                	hour = res.getHours(),
+                	minute = res.getMinutes();
+                if (month < 10) {
+                    month = '0' + month;
+                }
+                if (day < 10) {
+                    day = '0' + day;
+                }
+                if (hour < 10) {
+                    hour = '0' + hour;
+                }
+                if (minute < 10) {
+                    minute = '0' + minute;
+                }
+                
+                let fullYear = [year, month, day].join('-');
+                let fullTime = [hour, minute].join(':');
+                return `${fullYear} ${fullTime}`;
 
 
-	subtract(num, val) {
-		if(!timeObjects.includes(val) || (num < 0) || isNaN(num)) {
-			throw new TypeError('Invalid input');
-		} else if (val === 'years') {
-			time.setFullYear(time.getFullYear() - num); 
-		} else if (val === 'months') {
-			time.setMonth(time.getMonth() - num); 
-		} else if (val === 'days') {
-			time.setDate(time.getDate() - num); 
-		} else if (val === 'hours') {
-			time.setHours(time.getHours() - num); 
-		} else if (val === 'minutes') {
-			time.setMinutes(time.getMinutes() - num); 
-		} 
-		return this;
-	},
+            }
 
-	 get value() {
-	 	let res = time,
-		year = String(res.getFullYear()),
-		month = String(res.getMonth() + 1),
-		day = String(res.getDate()),
-		hour = String(res.getHours()),
-		minute = String(res.getMinutes());
+    }
 
-		if(month.length < 2) {
-		month += '0';
-	} if (day.length < 2) {
-		day += '0';
-	} if (hour.length < 2) {
-		hour += '0';
-	} if (minute.length < 2) {
-		minute += '0';
-	}
-
-
-	let fullYear = [year, month, day].join('-');
-	let fullTime = [hour, minute].join(':');
-
-	
-	return `${fullYear} ${fullTime}`;
-	}
-	
-}
-
+    return time;
 };
